544176a4856ead7ff1920c26db1806e597401fc9
Andrew Ogden <andrewogden@bigpond.com>
2020-10-25 16:51:11 +1100

    cors policy stuff

--- NAT-Tracks/Startup.cs
+++ NAT-Tracks/Startup.cs
@@ -23,6 +23,7 @@ namespace NAT_Tracks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -33,7 +34,12 @@ namespace NAT_Tracks
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(builder => builder
+                 .AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+            app.UseMvc();
+                
             app.UseMvc();
         }
     }
